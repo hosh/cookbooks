d1f3e4f3598d29ae134640693cdd74ebbaf1a115
Ho-Sheng Hsiao <hosh@sparkfly.com>
2010-05-07 11:47:29 -0400

    [GENTOO] Added feature_getbinpkg recipe

---
+++ gentoo/recipes/feature_getbinpkg.rb
@@ -0,0 +1,25 @@
+#
+# Use this if you want portage to pass the -g flag to emerge (look for binary packages when emerging)
+#
+# Author:: Ho-Sheng Hsiao <hosh@sparkfly.com>
+# Source:: http://www.gentoo.org/doc/en/handbook/handbook-x86.xml?part=2&chap=3#doc_chap4 
+# Cookbook Name:: gentoo
+# Recipe:: feature_getbinpkg
+#
+# Copyright 2010, Sparkfly
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include_recipe 'gentoo::portage'
+
+portage_feature :getbinpkg
