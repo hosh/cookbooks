145f5248ee710cb9855de8a51648873409a221c0
Ho-Sheng Hsiao <hosh@sparkfly.com>
2010-05-10 12:03:01 -0400

    [GENTOO] Imported veszig/gentoo-cookbooks portage LWRP http://github.com/veszig/gentoo-cookbooks/tree/0cd06acc5b8e4b2bb855f51430b4a8a66de23023/gentoo

---
+++ gentoo/libraries/monkey_patch_portage.rb
@@ -0,0 +1,52 @@
+
+# Reopens and overrides Chef::Provider::Package::Portage
+# Works with Chef 0.8.10
+
+class Chef
+  class Provider
+    class Package
+      class Portage < Chef::Provider::Package
+
+        # Override this in your libraries/
+        def default_emerge_options(options)
+          "--color n --nospinner --quiet#{options}"
+        end
+        
+        # Override this in your libraries/
+        def default_emerge_install_options
+          default_emerge_options('-g --binpkg-respect-use y')
+        end
+
+        # Override this in your libraries/
+        def default_emerge_upgrade_options
+          default_emerge_options('-g --binpkg-respect-use y -u -D')
+        end
+
+        def emerge_package(pkg, emerge_options = nil)
+          emerge_options ||= default_emerge_install_options
+          "emerge #{emerge_options}#{expand_options(@new_resource.options)} #{pkg}"
+        end
+
+        def full_package_name(name, version)
+          if(version =~ /^\~(.+)/)        
+            # If we start with a tilde      
+            "~#{name}-#{$1}"          
+          else
+            "=#{name}-#{version}"     
+          end
+        end
+
+        def install_package(name, version)
+          run_command_with_systems_locale( 
+            :command => emerge_package(full_package_name(name, version), default_emerge_install_options))
+        end
+
+        def upgrade_package(name, version)
+          run_command_with_systems_locale( 
+            :command => emerge_package(full_package_name(name, version), default_emerge_upgrade_options))
+        end
+      end
+    end
+  end
+end
+
