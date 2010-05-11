0f315e0516be1713baa5fb5be328d32417f317ac
Ho-Sheng Hsiao <hosh@sparkfly.com>
2010-05-11 18:24:57 -0400

    [MONIT] Tweaked monit attribute

--- monit/attributes/monit.rb
+++ monit/attributes/monit.rb
@@ -1,3 +1,4 @@
-set_unless[:monit][:mailservers] = ["smtp.#{node[:domain]}"]
+#set_unless[:monit][:mailservers] = ["smtp.#{node[:domain]}"]
+set_unless[:monit][:mailservers] = []
 set_unless[:monit][:alert_mail_from] = "monit@#{node[:fqdn]}"
 set_unless[:monit][:alert_mail_to] = "root@#{node[:domain]}"
