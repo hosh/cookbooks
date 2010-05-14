638a7b6db7ac9e772ecab5f1f8a692b27f0b759d
Ho-Sheng Hsiao <hosh@sparkfly.com>
2010-05-14 17:13:20 -0400

    [COUCHDB/MONIT] Added couchdb::monit_server recipe. This one needs some tweaking.

---
+++ couchdb/recipes/monit_server.rb
@@ -0,0 +1,28 @@
+# 
+# Use this to monitor chef-server with monit
+#
+# Author:: Ho-Sheng Hsiao <hosh@sparkfly.com>
+# Cookbook Name:: chef
+# Recipe:: chef::monit_server
+#
+# Copyright 2010 Sparkfly
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if node.recipe?('monit')
+  monit_http_service 'couchdb' do
+    process :listen_ips => [%w(127.0.0.1 5984)],
+      :pid_file => '/var/run/couchdb/couchdb.pid',
+      :timout_before_restart => '30'
+  end
+end
